4677470dce99c8c6104bfcf49bb5eaee62d23b1c
jessicaluhrmann <jessica.luhrmann@xplortechnologies.com>
2023-10-24 15:16:24 +1000

    Add project files.

---
+++ OOYA/Profile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DatingProfile
+{
+    public class Profile
+    {
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        public Profile(string name, int age, string city, string country, string pronouns = "they/them")
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.hobbies = new string[0];
+        }
+
+        public string ViewProfile()
+        {
+            var bio = $"Name: {name}\nAge: {age}\nPronouns: {pronouns}\nLocation: {city}, {country}";
+            bio += "\nHobbies:\n";
+            foreach (var hobby in hobbies)
+            {
+                bio += $"- {hobby}\n";
+            }
+            return bio;
+        }
+
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+    }
+}
---
+++ OOYA/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DatingProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Sam Drakkila", 30, "NYC", "USA", "he/him");
+            sam.SetHobbies(new string[] { "listening to audiobooks and podcasts", "playing rec sports like bowling and kickball", "writing a speculative fiction novel", "reading advice columns" });
+            Console.WriteLine(sam.ViewProfile());
+        }
+    }
+}
